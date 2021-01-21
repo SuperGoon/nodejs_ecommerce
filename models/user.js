ef7717030942c45cd6ee0644fae1f4565762cdb2
super goon <binhminh3426@gmail.com>
2021-01-21 10:26:28 +0700

    update

--- models/user.js
+++ models/user.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true, minLength: 8 },
     id: { type: String },
     role: { type: Number, default: 0 },
     cart: { type: Array },
